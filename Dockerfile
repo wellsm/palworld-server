0cd7184322b777a07f570912b75b967232c47952
Wellington Martins <wellingtonsdsm@gmail.com>
2026-02-04 18:22:31 -0300

    add retrying process on start.sh

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,12 @@ ENV STEAMAPPDIR=/palworld
 ENV STEAMUSER=steam
 ENV STEAMCMDDIR=/steamcmd
 
+RUN apt-get update && apt-get install -y locales && \
+    locale-gen en_US.UTF-8
+
+ENV LANG=en_US.UTF-8
+ENV LC_ALL=en_US.UTF-8
+
 RUN apt-get update && apt-get install -y \
     wget \
     ca-certificates \
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,9 +1,49 @@
 #!/bin/bash
+set -e
 
-/steamcmd/steamcmd.sh \
-  +force_install_dir /palworld \
-  +login anonymous \
-  +app_update 2394010 validate \
-  +quit
+export HOME=/home/steam
+export USER=steam
 
+mkdir -p "$HOME/.steam"
+
+MAX_RETRIES=5
+RETRY_DELAY=20
+
+attempt=1
+
+while [ $attempt -le $MAX_RETRIES ]; do
+  echo "[SteamCMD] Attempt $attempt / $MAX_RETRIES"
+
+  if /steamcmd/steamcmd.sh \
+      +@ShutdownOnFailedCommand 1 \
+      +@NoPromptForPassword 1 \
+      +force_install_dir /palworld \
+      +login anonymous \
+      +app_update 2394010 validate \
+      +quit; then
+
+    echo "[SteamCMD] Update OK"
+
+    break
+  fi
+
+  echo "[SteamCMD] Failed, cleaning cache..."
+
+  rm -rf /home/steam/.steam
+  rm -rf /palworld/steamapps
+
+  echo "[SteamCMD] Retrying in ${RETRY_DELAY}s..."
+
+  sleep $RETRY_DELAY
+
+  attempt=$((attempt + 1))
+done
+
+if [ $attempt -gt $MAX_RETRIES ]; then
+  echo "[SteamCMD] Failed after $MAX_RETRIES attempts"
+  exit 1
+fi
+
+echo "[Palworld] Starting server..."
 exec ./PalServer.sh -useperfthreads -NoAsyncLoadingThread -UseMultithreadForDS
+
